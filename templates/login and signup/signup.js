512201a4533fc73859c196441a9272f1cd2081a6
wagyu <wahyuramadhan.s31@student.uir.ac.id>
2025-01-23 18:17:02 +0700

    10

--- templates/login and signup/signup.js
+++ templates/login and signup/signup.js
@@ -1,36 +1,20 @@
 import React, { useState } from "react";
-import {
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import styles from "../assets/style";
+import { Text, View, TextInput, TouchableOpacity, Alert } from "react-native";
 import api from "../utils/api";
+import styles from "../assets/style";
 
 export default function SignUp({ navigation }) {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSignUp = async () => {
-    if (!email || !password) {
-      Alert.alert("Error", "Please fill in all fields.");
-      return;
-    }
-
     try {
-      const response = await api.post("/register", {
-        NamaPengguna: email,
-        KataSandi: password,
-      });
-      Alert.alert("Success", "Account created successfully!");
-      navigation.navigate("Registration", { userId: response.data.userId }); // Navigate to the next screen
+      await api.post("/register", { NamaPengguna: email, KataSandi: password, NamaToko: name });
+      Alert.alert("Sign Up Success", "Account created successfully!");
+      navigation.navigate("LogIn");
     } catch (error) {
-      Alert.alert(
-        "Error",
-        error.response?.data?.message || "Failed to create account."
-      );
+      Alert.alert("Sign Up Failed", error.response?.data?.message || "Error creating account.");
     }
   };
 
@@ -39,15 +23,19 @@ export default function SignUp({ navigation }) {
       <Text style={styles.title}>Sign Up</Text>
       <TextInput
         style={styles.input}
+        placeholder="Store Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
         placeholder="Email"
-        placeholderTextColor="#000"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
-        placeholderTextColor="#000"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
